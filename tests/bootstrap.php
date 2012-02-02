64e6a8f41cd087db87ca04c5e80080b59b42df74
ttsuruoka <tatsuya.tsuruoka@gmail.com>
2012-02-02 19:33:57 +0900

    不要な行を削除

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,3 @@
 <?php
 
 error_reporting( E_ALL | E_STRICT );
-
-// require_once __DIR__.'/vendor/SplClassLoader.php';
-
-//$loader = new SplClassLoader('Diggin', dirname(__DIR__).'/src/');
-//$loader->register();
-
