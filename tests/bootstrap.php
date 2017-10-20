8f13180fa5e7c41d2f3a8622387e5367ae7d7ac1
DQNEO <dqneoo@gmail.com>
2017-10-20 17:52:44 +0900

    use multi phpunit versions (#27)
    * use multi phpunit versions
    
    * remove unneeded class aliases

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,12 +4,3 @@ error_reporting(E_ALL | E_STRICT);
 define('ROOT_DIR', dirname(dirname(__FILE__)).'/');
 define('APP_DIR', ROOT_DIR.'app/');
 require_once ROOT_DIR.'dietcake.php';
-
-// backward compatibility for php 5.5 and low (with phpunit < v.6)
-if (!class_exists('\PHPUnit\Framework\TestCase') && class_exists('\PHPUnit_Framework_TestCase')) {
-    class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
-}
-
-if (!class_exists('\PHPUnit_Framework_TestCase') && class_exists('\PHPUnit\Framework\TestCase')) {
-    class_alias('\PHPUnit\Framework\TestCase', '\PHPUnit_Framework_TestCase');
-}
